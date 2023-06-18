0ff242b05f8cfb2909475254727c17b2e42d8ca2
writetofahim <writetofahimahammad@gmail.com>
2023-06-18 23:12:30 +0600

    external module done

--- External_Module/index.js
+++ External_Module/index.js
@@ -1,1 +1,5 @@
-console.log("hello world");
+const getRandomFruitsName = require("random-fruits-name");
+const getMoviesName = require("movies-names");
+console.log(getRandomFruitsName("es"));
+console.log(getRandomFruitsName("es"));
+console.log(getMoviesName.random());
