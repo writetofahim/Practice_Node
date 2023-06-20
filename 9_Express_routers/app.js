adaa68ac562a5eeff804bbc24245beb4c330c80b
writetofahim <writetofahimahammad@gmail.com>
2023-06-21 01:18:39 +0600

    emergency fixing

--- 9_Express_routers/app.js
+++ 9_Express_routers/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const userRoute = require("./routes/users.route.js");
 
-app.use(userRoute);
+// if specified route
+app.use("/api/user", userRoute);
 
+// if root route
 app.get("/", (req, res) => {
   res.send("<h1>I am home </h1>");
 });
 
+// if unknown route
 app.use((req, res) => {
   res.send("<h1>404 not found!</h1>");
 });
